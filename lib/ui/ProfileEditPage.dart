e4d135e35b665cfa5f0b6a3e0936b96459db53f0
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-01-08 17:32:52 +0200

    Retrieve name/phone/birthdate values, just alert them for now.

--- lib/ui/ProfileEditPage.dart
+++ lib/ui/ProfileEditPage.dart
@@ -8,6 +8,19 @@ class ProfileEditPage extends StatefulWidget {
 }
 
 class _ProfileEditPageState extends State<ProfileEditPage> {
+  final _nameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _birthDateController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    _nameController.dispose();
+    _phoneController.dispose();
+    _birthDateController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,17 +33,20 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             TextField(
+                controller: _nameController,
                 autofocus: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Please, type your name',
                     labelText: 'Name:')),
             TextField(
+                controller: _phoneController,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Please, type your name',
                     labelText: 'Phone:')),
             TextField(
+                controller: _birthDateController,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Please, type your date of birth',
@@ -40,13 +56,25 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
               children: <Widget>[
                 RaisedButton(
                   child: const Text('Save'),
-                  onPressed: (){
-                    //TODO: Implement save profile
+                  onPressed: () {
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        var name = _nameController.text;
+                        var phone = _phoneController.text;
+                        var birthDate = _birthDateController.text;
+                        return AlertDialog(
+                          // Retrieve the text the user has typed in using our
+                          // TextEditingController
+                          content: Text('Name: $name\nPhone: $phone\nBirthdate: $birthDate'),
+                        );
+                      },
+                    );
                   },
                 ),
                 RaisedButton(
                   child: const Text('Delete'),
-                  onPressed: (){
+                  onPressed: () {
                     //TODO: Implement delete profile
                   },
                 ),
