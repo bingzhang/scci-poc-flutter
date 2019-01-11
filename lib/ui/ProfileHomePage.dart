adb285174c18ccf3c0bd35252f9130f2672ab64f
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-11 14:38:13 +0200

    Make more user friendly ui for home page

--- lib/ui/ProfileHomePage.dart
+++ lib/ui/ProfileHomePage.dart
@@ -49,60 +49,40 @@ class _ProfileHomePageState extends State<ProfileHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text("Profile Demo"),
       ),
-      body: Center(
+      body: ListView(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.topCenter,
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                children: <Widget>[
-                  Flexible(
-                    child: TextField(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
                       controller: _hostController,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Please, type your host',
-                            labelText: 'Host:')),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Please, type your host',
+                          labelText: 'Host:')),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: RaisedButton(
+                    child: const Text('Save'),
+                    onPressed: () => performSaveHostAddress(_hostController.text),
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 4.0),
-                    child: RaisedButton(
-                      child: const Text('Save'),
-                      onPressed: () => performSaveHostAddress(_hostController.text),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            RaisedButton(
+          ),
+          Center(
+            heightFactor: 10,
+            child: RaisedButton(
               child: const Text('Profile'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfileEditPage()),
-                );
-              },
+              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileEditPage())),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
