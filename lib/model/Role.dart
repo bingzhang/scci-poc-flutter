2328549151647cff8a1aaee615369cdecbda65e0
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-15 13:38:21 +0200

    Remove commented unused code

--- lib/model/Role.dart
+++ lib/model/Role.dart
@@ -1,8 +1,1 @@
 enum Role { student, staff, other, unknown }
-
-//Role fromString(String roleString) {
-//  if (roleString == null || roleString.isEmpty) {
-//    return Role.unknown;
-//  }
-//  return Role.values.firstWhere((r) => r.toString() == 'Role.' + roleString);
-//}
