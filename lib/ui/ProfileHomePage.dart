308fa52cf17830aaa5a528820c0fccacb3f8720b
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-22 10:53:53 +0200

    Code alignment

--- lib/ui/ProfileHomePage.dart
+++ lib/ui/ProfileHomePage.dart
@@ -53,60 +53,63 @@ class _ProfileHomePageState extends State<ProfileHomePage> {
       ),
       body: Center(
           child: Padding(
-            padding: EdgeInsets.fromLTRB(0,UiConstants.HOME_TOP_SPACING, 0, 0),
-    child:ListView(
-        children: <Widget>[
-          RoundedImageButton(
-              visible: true,
-              imageRelativePath: 'images/campus.jpg',
-              sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-              onTapGesture: () {
-                _openWeb("Getting Around", "https://goo.gl/maps/vc7DRLgiMM22");
-              }),
-          RoundedImageButton(
-              visible: (_userRole == Role.student),
-              imageRelativePath: 'images/Bardeen-Quad2.jpg',
-              sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-              onTapGesture: () {
-                _openWeb("General Info",
-                    "http://catalog.illinois.edu/general-information/");
-              }),
-          RoundedImageButton(
-              visible: true,
-              imageRelativePath: 'images/athletics.jpg',
-              sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-              onTapGesture: () {
-                _openWeb("Athletics", "https://fightingillini.com/");
-              }),
-          RoundedImageButton(
-              visible:
-                  ((_userRole == Role.student) || (_userRole == Role.staff)),
-              imageRelativePath: 'images/krannert.jpg',
-              sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-              onTapGesture: () {
-                _openWeb("Events", "https://krannertcenter.com/calendar");
-              }),
-          RoundedImageButton(
-              visible: (_userRole == Role.student),
-              imageRelativePath: 'images/Altgeld.jpg',
-              sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-              onTapGesture: () {
-                _openWeb("Schedule",
-                    "https://courses.illinois.edu/schedule/DEFAULT/DEFAULT");
-              }),
-          RoundedImageButton(
-            visible: true,
-            imageRelativePath: 'images/Illinois.jpg',
-            sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
-            onTapGesture: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProfileEditPage()))
-                .then((value) => _loadUser()),
-          ),
-        ],
-    )
-      )),
+              padding:
+                  EdgeInsets.fromLTRB(0, UiConstants.HOME_TOP_SPACING, 0, 0),
+              child: ListView(
+                children: <Widget>[
+                  RoundedImageButton(
+                      visible: true,
+                      imageRelativePath: 'images/campus.jpg',
+                      sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                      onTapGesture: () {
+                        _openWeb("Getting Around",
+                            "https://goo.gl/maps/vc7DRLgiMM22");
+                      }),
+                  RoundedImageButton(
+                      visible: (_userRole == Role.student),
+                      imageRelativePath: 'images/Bardeen-Quad2.jpg',
+                      sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                      onTapGesture: () {
+                        _openWeb("General Info",
+                            "http://catalog.illinois.edu/general-information/");
+                      }),
+                  RoundedImageButton(
+                      visible: true,
+                      imageRelativePath: 'images/athletics.jpg',
+                      sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                      onTapGesture: () {
+                        _openWeb("Athletics", "https://fightingillini.com/");
+                      }),
+                  RoundedImageButton(
+                      visible: ((_userRole == Role.student) ||
+                          (_userRole == Role.staff)),
+                      imageRelativePath: 'images/krannert.jpg',
+                      sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                      onTapGesture: () {
+                        _openWeb(
+                            "Events", "https://krannertcenter.com/calendar");
+                      }),
+                  RoundedImageButton(
+                      visible: (_userRole == Role.student),
+                      imageRelativePath: 'images/Altgeld.jpg',
+                      sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                      onTapGesture: () {
+                        _openWeb("Schedule",
+                            "https://courses.illinois.edu/schedule/DEFAULT/DEFAULT");
+                      }),
+                  RoundedImageButton(
+                    visible: true,
+                    imageRelativePath: 'images/Illinois.jpg',
+                    sizeRatio: UiConstants.HOME_BUTTONS_ASPECT_RATIO,
+                    onTapGesture: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfileEditPage()))
+                        .then((value) => _loadUser()),
+                  ),
+                ],
+              ))),
     );
-
   }
 
   void _openWeb(String title, String url) {
