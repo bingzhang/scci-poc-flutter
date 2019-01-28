0eac5e7a355d3d980bb1d013d9fb32b3fe36ca87
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-28 16:41:05 +0200

    Extract widgets creation in single place. Remove repeated code.

--- lib/ui/panels/static/StaticFormPanel2.dart
+++ lib/ui/panels/static/StaticFormPanel2.dart
@@ -3,13 +3,11 @@
  */
 
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:profile_demo/utility/StaticHelper.dart';
 
 class StaticFormPanel2 extends StatelessWidget {
-  final EdgeInsets widgetsMargin = EdgeInsets.all(5.0);
-  final widgetPortionCount = 10;
+  final widgetsCount = 10;
   final List<Widget> panelWidgets = List();
-  final Random randomInstance = new Random();
 
   @override
   Widget build(BuildContext context) {
@@ -22,86 +20,11 @@ class StaticFormPanel2 extends StatelessWidget {
   }
 
   List<Widget> _buildPanelWidgets() {
-    _buildEditWidgets();
-    _buildRadioWidgets();
-    _buildCheckboxWidgets();
-    _buildListWidgets();
-    _buildDropdownWidgets();
+    panelWidgets.addAll(StaticHelper.buildEditWidgets(widgetsCount));
+    panelWidgets.addAll(StaticHelper.buildRadioWidgets(widgetsCount));
+    panelWidgets.addAll(StaticHelper.buildCheckboxWidgets(widgetsCount));
+    panelWidgets.addAll(StaticHelper.buildListWidgets(widgetsCount));
+    panelWidgets.addAll(StaticHelper.buildDropdownWidgets(widgetsCount));
     return panelWidgets;
-  }
-
-  void _buildEditWidgets() {
-    for (int index = 1; index <= widgetPortionCount; index++) {
-      Container editContainer = Container(
-          margin: widgetsMargin,
-          child: TextFormField(
-              controller: TextEditingController(),
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'EditControl $index',
-                  labelText: 'Edit Control $index')));
-      panelWidgets.add(editContainer);
-    }
-  }
-
-  void _buildRadioWidgets() {
-    for (int index = 1; index <= widgetPortionCount; index++) {
-      Container radioContainer = Container(
-        margin: widgetsMargin,
-        child: RadioListTile(
-            title: Text('Radio $index'), groupValue: null, value: index, onChanged: null),
-      );
-      panelWidgets.add(radioContainer);
-    }
-  }
-
-  void _buildCheckboxWidgets() {
-    for (int index = 1; index <= widgetPortionCount; index++) {
-      Container checkboxContainer = Container(
-        margin: widgetsMargin,
-        child: CheckboxListTile(
-            title: Text('Checkbox $index'),
-            value: randomInstance.nextBool(),
-            onChanged: null),
-      );
-      panelWidgets.add(checkboxContainer);
-    }
-  }
-
-  void _buildListWidgets() {
-    for (int listViewIndex = 1;
-        listViewIndex <= widgetPortionCount;
-        listViewIndex++) {
-      Column singleListView = Column(
-          children: List.generate(4, (int itemIndex) {
-        if (itemIndex == 0) {
-          return Text('List $listViewIndex');
-        }
-        return Card(
-            child: ListTile(
-                leading: Icon(Icons.format_list_bulleted),
-                title: Text('Item $itemIndex')));
-      }));
-      panelWidgets.add(singleListView);
-    }
-  }
-
-  void _buildDropdownWidgets() {
-    var dropDownItemValues = <String>['A', 'B', 'C', 'D'];
-    for (int index = 1; index <= widgetPortionCount; index++) {
-      var randomValue = randomInstance.nextInt(dropDownItemValues.length);
-      DropdownButton<String> dropDownButton = DropdownButton<String>(
-        value: dropDownItemValues[randomValue],
-        items: dropDownItemValues.map((String value) {
-          return new DropdownMenuItem<String>(
-            value: value,
-            child: new Text(value),
-          );
-        }).toList(),
-        onChanged: (_) {},
-      );
-      panelWidgets.add(dropDownButton);
-    }
   }
 }
---
+++ lib/utility/StaticHelper.dart
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2019 UIUC. All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class StaticHelper {
+  static final EdgeInsets widgetsMargin = EdgeInsets.all(5.0);
+  static final Random randomInstance = new Random();
+
+  static List<Widget> buildEditWidgets(int widgetsCount) {
+    List<Widget> editWidgets = List();
+    for (int index = 1; index <= widgetsCount; index++) {
+      Container editContainer = Container(
+          margin: widgetsMargin,
+          child: TextFormField(
+              controller: TextEditingController(),
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'EditControl $index',
+                  labelText: 'Edit Control $index')));
+      editWidgets.add(editContainer);
+    }
+    return editWidgets;
+  }
+
+  static List<Widget> buildRadioWidgets(int widgetsCount) {
+    List<Widget> radioWidgets = List();
+    for (int index = 1; index <= widgetsCount; index++) {
+      Container radioContainer = Container(
+        margin: widgetsMargin,
+        child: RadioListTile(
+            title: Text('Radio $index'),
+            groupValue: null,
+            value: index,
+            onChanged: null),
+      );
+      radioWidgets.add(radioContainer);
+    }
+    return radioWidgets;
+  }
+
+  static List<Widget> buildCheckboxWidgets(int widgetsCount) {
+    List<Widget> checkboxWidgets = List();
+    for (int index = 1; index <= widgetsCount; index++) {
+      Container checkboxContainer = Container(
+        margin: widgetsMargin,
+        child: CheckboxListTile(
+            title: Text('Checkbox $index'),
+            value: randomInstance.nextBool(),
+            onChanged: null),
+      );
+      checkboxWidgets.add(checkboxContainer);
+    }
+    return checkboxWidgets;
+  }
+
+  static List<Widget> buildListWidgets(int widgetsCount) {
+    List<Widget> listWidgets = List();
+    for (int listViewIndex = 1;
+        listViewIndex <= widgetsCount;
+        listViewIndex++) {
+      Column singleListView = Column(
+          children: List.generate(4, (int itemIndex) {
+        if (itemIndex == 0) {
+          return Text('List $listViewIndex');
+        }
+        return Card(
+            child: ListTile(
+                leading: Icon(Icons.format_list_bulleted),
+                title: Text('Item $itemIndex')));
+      }));
+      listWidgets.add(singleListView);
+    }
+    return listWidgets;
+  }
+
+  static List<Widget> buildDropdownWidgets(int widgetsCount) {
+    List<Widget> dropdownWidgets = List();
+    var dropDownItemValues = <String>['A', 'B', 'C', 'D'];
+    for (int index = 1; index <= widgetsCount; index++) {
+      var randomValue = randomInstance.nextInt(dropDownItemValues.length);
+      DropdownButton<String> dropDownButton = DropdownButton<String>(
+        value: dropDownItemValues[randomValue],
+        items: dropDownItemValues.map((String value) {
+          return new DropdownMenuItem<String>(
+            value: value,
+            child: new Text(value),
+          );
+        }).toList(),
+        onChanged: (_) {},
+      );
+      dropdownWidgets.add(dropDownButton);
+    }
+    return dropdownWidgets;
+  }
+}
