e40c2fd2991c7be7f344876f24329eea49d2fb5f
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-31 09:56:03 +0200

    Create new empty panels for student. To be implemented ...

---
+++ lib/ui/panels/student/StudentCampusPanel.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 UIUC. All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+
+class StudentCampusPanel extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/panels/student/StudentSchedulePanel.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 UIUC. All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+
+class StudentSchedulePanel extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
