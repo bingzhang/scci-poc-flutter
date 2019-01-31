f4ae44b000d8aa5466e722306945702b172fbbb6
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-01-31 16:05:23 +0200

    Layout Student Home Panel

--- lib/ui/panels/student/StudentHomePanel.dart
+++ lib/ui/panels/student/StudentHomePanel.dart
@@ -42,7 +42,7 @@ class StudentHomePanel extends StatelessWidget {
                   Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
-                      padding: EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                      padding: EdgeInsets.only(left: 10.0, top: 10.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
@@ -58,11 +58,49 @@ class StudentHomePanel extends StatelessWidget {
                   Align(
                     alignment: Alignment.topRight,
                     child: Padding(
-                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 10.0, 0.0),
+                      padding: EdgeInsets.only(top: 10.0, right: 10.0),
                       child: Column(
                         children: <Widget>[
                           Image.asset('images/icon-weather.png', height: 50.0),
                           Text('30˚ F')
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 10.0, bottom: 10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Good Morning, Alex!',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(right: 10.0),
+                                child: Image.asset('images/icon-schedule.png',
+                                    height: 50.0),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Next Event 9:30am',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text('TE 401: Intro to Design Thinking'),
+                                  Text('Noble Hall, Room 211')
+                                ],
+                              )
+                            ],
+                          )
                         ],
                       ),
                     ),
@@ -126,7 +164,7 @@ class StudentHomePanel extends StatelessWidget {
               child: Container(
                 height: 50,
                 child: Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
+                  padding: EdgeInsets.only(left: 20.0, top: 15.0, bottom: 15.0),
                   child: Text(
                     'Life on Campus',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
@@ -140,7 +178,7 @@ class StudentHomePanel extends StatelessWidget {
               child: Container(
                 height: 50,
                 child: Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
+                  padding: EdgeInsets.only(left: 20.0, top: 15.0, bottom: 15.0),
                   child: Text(
                     'News + Events',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
@@ -159,7 +197,7 @@ class StudentHomePanel extends StatelessWidget {
               child: Container(
                 height: 50,
                 child: Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
+                  padding: EdgeInsets.only(left: 20.0, top: 15.0, bottom: 15.0),
                   child: Text(
                     'Athletics + Campus Venues',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
