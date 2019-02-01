62e32fb3e80484511c77bb2bac7de13cc02bf50d
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-01 14:58:01 +0200

    LifeInCampus panels layout is done.

--- lib/ui/panels/student/StudentLifeInCampusPanel.dart
+++ lib/ui/panels/student/StudentLifeInCampusPanel.dart
@@ -5,6 +5,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:profile_demo/ui/widgets/HeaderAppBar.dart';
+import 'package:profile_demo/ui/widgets/HorizontalDivider.dart';
+import 'package:profile_demo/ui/widgets/Option.dart';
+import 'package:profile_demo/ui/widgets/RibbonButton.dart';
+import 'package:profile_demo/ui/widgets/SearchBar.dart';
+import 'package:profile_demo/ui/widgets/TimeAndWhetherHeader.dart';
 
 class StudentLifeOnCampusPanel extends StatelessWidget {
   @override
@@ -12,7 +17,72 @@ class StudentLifeOnCampusPanel extends StatelessWidget {
     return Scaffold(
         appBar: HeaderAppBar(context: context),
         body: Column(
-          children: <Widget>[Text('Slide 8 right')],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "images/header_about.jpg",
+                      ),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.2), BlendMode.dstATop)),
+                ),
+                child: Column(
+                  children: <Widget>[TimeAndWhetherHeader()],
+                ),
+              ),
+            ),
+            HorizontalDivider(),
+            RibbonButton(
+              title: 'Life on Campus',
+            ),
+            HorizontalDivider(),
+            Expanded(
+              flex: 3,
+              child: GridView.count(
+                crossAxisCount: 3,
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  Option(
+                    title: 'To Do List',
+                  ),
+                  Option(
+                    title: 'Dining',
+                  ),
+                  Option(
+                    title: 'Events',
+                  ),
+                  Option(
+                    title: 'Navigation',
+                  ),
+                  Option(
+                    title: 'Social',
+                  ),
+                  Option(
+                    title: 'Athletics',
+                  ),
+                  Option(
+                    title: 'Navigation',
+                  ),
+                  Option(
+                    title: 'News',
+                  ),
+                  Option(
+                    title: 'Schedule',
+                  ),
+                  Option(
+                    title: 'Grades',
+                  )
+                ],
+              ),
+            ),
+            SearchBar()
+          ],
         ));
   }
 }
---
+++ lib/ui/widgets/Option.dart
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 UIUC. All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+
+class Option extends StatelessWidget {
+  final String title;
+  final GestureTapCallback tapCallback;
+
+  Option({this.title, this.tapCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: tapCallback,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'images/icon-option-placeholder.png',
+            height: 62,
+            width: 62,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontFamily: 'Avenir'),
+          )
+        ],
+      ),
+    );
+  }
+}
