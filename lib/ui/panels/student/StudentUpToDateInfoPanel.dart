a05dd6fdeb062ab20a381b6bc1868eecb113971f
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-02-01 14:07:32 +0200

    Navigate to Schedule panel from UpToDateInfo.

--- lib/ui/panels/student/StudentUpToDateInfoPanel.dart
+++ lib/ui/panels/student/StudentUpToDateInfoPanel.dart
@@ -7,6 +7,8 @@ import 'package:profile_demo/ui/widgets/HeaderAppBar.dart';
 import 'package:profile_demo/ui/widgets/RibbonButton.dart';
 import 'package:profile_demo/ui/widgets/SearchBar.dart';
 import 'package:profile_demo/ui/widgets/TimeAndWhetherHeader.dart';
+import 'package:profile_demo/ui/navigation/NavigationRouter.dart';
+import 'StudentSchedulePanel.dart';
 
 class StudentUpToDateInfoPanel extends StatelessWidget {
   @override
@@ -82,7 +84,7 @@ class StudentUpToDateInfoPanel extends StatelessWidget {
             Container(height: 1, color: Colors.black26,),
             RibbonButton(title: 'Assignments Due Next',),
             Container(height: 1, color: Colors.black26,),
-            RibbonButton(title: "Today's Schedule",),
+            RibbonButton(title: "Today's Schedule", gestureTapCallback: NavigationRouter.openPanel(context, StudentSchedulePanel()),),
             SearchBar()
           ])
         );
