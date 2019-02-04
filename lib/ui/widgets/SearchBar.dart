d25b8e85eb0a0941035e35f1816dee1380e304cd
Unknown <tobachvarov@gmail.com>
2019-02-04 12:40:22 +0200

    Auto stash before merge of "master" and "origin/master"
    Update SearchBar widget

--- lib/ui/widgets/SearchBar.dart
+++ lib/ui/widgets/SearchBar.dart
@@ -5,13 +5,15 @@
 import 'package:flutter/material.dart';
 import 'package:profile_demo/ui/navigation/NavigationRouter.dart';
 import 'package:profile_demo/ui/panels/ProfileEditPanel.dart';
+import 'package:profile_demo/utility/Utils.dart';
 
 class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 15),
+        padding: EdgeInsets.symmetric(horizontal: 3, vertical: 4),
         color: Colors.black26,
+//        color: Color.fromARGB(0, 188, 192, 195),
         child: Row(children: <Widget>[
           GestureDetector(
             onTap: NavigationRouter.openPanel(context, ProfileEditPanel()),
@@ -22,7 +24,22 @@ class SearchBar extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Expanded(child: TextFormField()),
+          Expanded(child:Padding(
+          padding: EdgeInsets.symmetric(horizontal: 3, vertical: 4),
+          child:Ink(
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 134, 134, 134),
+                shape: UiConstants.roundedButtonBoxShape,
+                borderRadius: BorderRadius.all(Radius.circular(25.0))),
+            child:Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        border: InputBorder.none
+    )               ,
+    )           )
+          ))),
           Image.asset('images/icon-search.png',
               width: 42, height: 42, fit: BoxFit.cover),
         ]));
