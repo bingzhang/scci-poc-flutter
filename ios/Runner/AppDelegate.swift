7d124e925e700628270fc40b51bf1b07d790af85
Todor Bachvarov <tobachvarov@gmail.com>
2019-02-18 19:29:01 +0200

    Setup iOS Map Api Key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    //TODO KEY must be changed for each client (this is debug key)
+    let googleApiKey = "AIzaSyDzu401K9d7fzJjJ18_VVP_VvWAuFhi29g";
+    GMSServices.provideAPIKey(googleApiKey);
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
