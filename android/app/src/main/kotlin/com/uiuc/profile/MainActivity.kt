0cd4705b13691d3a72fefdfe7880d446b185dc08
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-02-19 14:54:05 +0200

    Listen for ‘com.inabyte.profile_demo/navive_call’ channel in Android native app, do nothing for now.

--- android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
+++ android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
@@ -4,10 +4,31 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.StringCodec
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "com.inabyte.profile_demo/navive_call"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    MethodChannel(flutterView, CHANNEL).setMethodCallHandler(
+                      object : MethodCallHandler {
+                          override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                              if(call.method.equals("indoorMaps")) {
+                                  launchIndoorMaps()
+                              }
+                          }
+                      })
   }
+
+  private fun launchIndoorMaps() {
+    //val intent = Intent(this, SecondActivity::class.java)
+    //startActivity(intent)
+  }
+
 }
