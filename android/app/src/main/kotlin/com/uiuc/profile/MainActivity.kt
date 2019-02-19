6ef3e6475b81496f2f71da0de8b1269f33137661
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-19 18:05:29 +0200

    Add basic MapsIndoors activity that will contain map

--- android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
+++ android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.uiuc.profile
 
+import android.content.Intent
 import android.os.Bundle
-import com.mapsindoors.mapssdk.MapsIndoors
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -14,7 +14,6 @@ class MainActivity : FlutterActivity() {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
 
-        initMapsIndoors()
         MethodChannel(flutterView, nativeChannel).setMethodCallHandler { methodCall, result ->
             if (methodCall.method == "indoorMaps") {
                 launchIndoorMaps()
@@ -25,14 +24,9 @@ class MainActivity : FlutterActivity() {
         }
     }
 
-    private fun initMapsIndoors() {
-        MapsIndoors.initialize(this, getString(R.string.mapsindoors_api_key))
-        MapsIndoors.setGoogleAPIKey(getString(R.string.google_maps_api_key))
-    }
-
     private fun launchIndoorMaps() {
-        //val intent = Intent(this, SecondActivity::class.java)
-        //startActivity(intent)
+        val intent = Intent(this, MapsIndoorsActivity::class.java)
+        startActivity(intent)
     }
 
 }
---
+++ android/app/src/main/kotlin/com/uiuc/profile/MapsIndoorsActivity.java
@@ -0,0 +1,77 @@
+package com.uiuc.profile;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.mapsindoors.mapssdk.Location;
+import com.mapsindoors.mapssdk.MapControl;
+import com.mapsindoors.mapssdk.MapsIndoors;
+import com.mapsindoors.mapssdk.dbglog;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class MapsIndoorsActivity extends FragmentActivity {
+    public static final String TAG = MapsIndoorsActivity.class.getSimpleName();
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap mGoogleMap;
+    private MapControl myMapControl;
+
+    private static final LatLng MAPSPEOPLE_CORPORATE_HQ_LOCATION = new LatLng(57.05813067, 9.95058065);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_indoors_layout);
+
+        // Enable MapsIndoors debug messages (console)
+        {
+            dbglog.useDebug(true);
+            dbglog.setCustomTagPrefix(TAG + "_");
+        }
+        init();
+        initMapFragment();
+    }
+
+    private void initMapFragment() {
+        mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment));
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(googleMap -> {
+                mGoogleMap = googleMap;
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MAPSPEOPLE_CORPORATE_HQ_LOCATION, 13.0f));
+                setupMapsIndoors();
+            });
+        }
+    }
+
+    private void setupMapsIndoors() {
+        myMapControl = new MapControl(this);
+        myMapControl.setGoogleMap(mGoogleMap, mapFragment.getView());
+        myMapControl.setOnMarkerClickListener(marker -> {
+            final Location loc = myMapControl.getLocation(marker);
+            if (loc != null) {
+                marker.showInfoWindow();
+            }
+            return true;
+        });
+        myMapControl.init(error -> {
+            if (error == null) {
+                runOnUiThread(() -> {
+                    myMapControl.selectFloor(0);
+                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(MAPSPEOPLE_CORPORATE_HQ_LOCATION, 19f));
+                });
+            }
+        });
+    }
+
+    private void init() {
+        MapsIndoors.initialize(
+                getApplicationContext(),
+                getString(R.string.mapsindoors_api_key)
+        );
+        MapsIndoors.setGoogleAPIKey(getString(R.string.google_maps_api_key));
+    }
+}
