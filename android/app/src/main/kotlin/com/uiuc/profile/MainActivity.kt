971781a17ddc2108f87b7d45de1cdafd8cb82316
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-19 16:18:40 +0200

    Integrate MapsIndoors. Only initialization for now ...

--- android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
+++ android/app/src/main/kotlin/com/uiuc/profile/MainActivity.kt
@@ -1,34 +1,38 @@
 package com.uiuc.profile
 
 import android.os.Bundle
+import com.mapsindoors.mapssdk.MapsIndoors
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugin.common.BasicMessageChannel
-import io.flutter.plugin.common.StringCodec
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-  private val CHANNEL = "com.uiuc.profile/native_call"
+class MainActivity : FlutterActivity() {
+    private val nativeChannel = "com.uiuc.profile/native_call"
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-    MethodChannel(flutterView, CHANNEL).setMethodCallHandler(
-                      object : MethodCallHandler {
-                          override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-                              if(call.method.equals("indoorMaps")) {
-                                  launchIndoorMaps()
-                              }
-                          }
-                      })
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
 
-  private fun launchIndoorMaps() {
-    //val intent = Intent(this, SecondActivity::class.java)
-    //startActivity(intent)
-  }
+        initMapsIndoors()
+        MethodChannel(flutterView, nativeChannel).setMethodCallHandler { methodCall, result ->
+            if (methodCall.method == "indoorMaps") {
+                launchIndoorMaps()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun initMapsIndoors() {
+        MapsIndoors.initialize(this, getString(R.string.mapsindoors_api_key))
+        MapsIndoors.setGoogleAPIKey(getString(R.string.google_maps_api_key))
+    }
+
+    private fun launchIndoorMaps() {
+        //val intent = Intent(this, SecondActivity::class.java)
+        //startActivity(intent)
+    }
 
 }
