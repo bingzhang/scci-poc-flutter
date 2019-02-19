38e46b2f2bc03725b78e889f198b1c23f99745f8
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-02-19 13:20:11 +0200

    Link Indoor Maps pod to iOS project, update google and indoor maps API keys.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import MapsIndoors
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +10,10 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     //TODO KEY must be changed for each client (this is debug key)
-    let googleApiKey = "AIzaSyDzu401K9d7fzJjJ18_VVP_VvWAuFhi29g";
+    let googleApiKey = "AIzaSyBOyWpp0LBhbaYiQUGUbc_YLcW8pRoA5Sc";
+    let mapsIndoorsKey = "56616c7bf5934092acfe0660"; // "57e4e4992e74800ef8b69718"
     GMSServices.provideAPIKey(googleApiKey);
+    MapsIndoors.provideAPIKey(mapsIndoorsKey, googleAPIKey: googleApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
