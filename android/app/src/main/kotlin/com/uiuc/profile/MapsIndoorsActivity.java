9131deae5e6b5a537492e219d9412a1c0aab2e2b
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-21 18:22:30 +0200

    Remove routing stuff. It is not working and crashing. Start tommorow on fresh.

--- android/app/src/main/kotlin/com/uiuc/profile/MapsIndoorsActivity.java
+++ android/app/src/main/kotlin/com/uiuc/profile/MapsIndoorsActivity.java
@@ -1,7 +1,6 @@
 package com.uiuc.profile;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,30 +10,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mapsindoors.mapssdk.Location;
-import com.mapsindoors.mapssdk.MPDirectionsRenderer;
-import com.mapsindoors.mapssdk.MPRoutingProvider;
 import com.mapsindoors.mapssdk.MapControl;
 import com.mapsindoors.mapssdk.MapsIndoors;
-import com.mapsindoors.mapssdk.Point;
-import com.mapsindoors.mapssdk.RoutingProvider;
-import com.mapsindoors.mapssdk.TravelMode;
 import com.mapsindoors.mapssdk.dbglog;
 
 import androidx.fragment.app.FragmentActivity;
 
 public class MapsIndoorsActivity extends FragmentActivity {
-    public static final String TAG = MapsIndoorsActivity.class.getSimpleName();
+    private static final String TAG = MapsIndoorsActivity.class.getSimpleName();
 
     private SupportMapFragment mapFragment;
 
     private GoogleMap googleMap;
     private MapControl mapControl;
-    private RoutingProvider routingProvider;
-    private MPDirectionsRenderer routingRenderer;
 
     private String userName;
     private Marker userMarker;
-
 
     private static final LatLng BUILDING_LOCATION = new LatLng(57.08585, 9.95751);
     private static final LatLng INITIAL_USER_LOCATION = new LatLng(57.087210, 9.958428);
@@ -84,10 +75,8 @@ public class MapsIndoorsActivity extends FragmentActivity {
     }
 
     private void setupMapsIndoors() {
-        routingProvider = new MPRoutingProvider();
         mapControl = new MapControl(this);
         mapControl.setGoogleMap(googleMap, mapFragment.getView());
-        routingRenderer = new MPDirectionsRenderer(this, googleMap, mapControl, null);
         mapControl.setOnMarkerClickListener(marker -> {
             final Location loc = mapControl.getLocation(marker);
             if (loc != null) {
@@ -102,33 +91,17 @@ public class MapsIndoorsActivity extends FragmentActivity {
                 runOnUiThread(() -> {
                     mapControl.selectFloor(0);
                     googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(BUILDING_LOCATION, 17f));
-
-                    // Animate route after 2 seconds
-                    new Handler(getMainLooper()).postDelayed(this::routing, 2000);
                 });
             }
         });
     }
 
-    private void routing() {
-        routingProvider.setTravelMode(TravelMode.TRAVEL_MODE_BICYCLING);
-        routingProvider.setOnRouteResultListener((route, error) -> {
-            if (route != null) {
-                routingRenderer.setRoute(route);
-                runOnUiThread(() -> routingRenderer.setRouteLegIndex(0));
-            }
-        });
-        Point origin = new Point(57.084171, 9.956732);
-        Point destination = new Point(57.08603248579481, 9.958068568122826);
-        routingProvider.query(origin, destination);
-    }
-
-    private void setupUserMarker(){
+    private void setupUserMarker() {
         userMarker = googleMap.addMarker(constructInitialMarker());
         userMarker.showInfoWindow();
     }
 
-    private MarkerOptions constructInitialMarker(){
+    private MarkerOptions constructInitialMarker() {
         MarkerOptions options = new MarkerOptions()
                 .position(INITIAL_USER_LOCATION)
                 .title(userName);
