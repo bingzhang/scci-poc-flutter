5b6b0df789db332b0e51ce5c4cb67a04dc54ba14
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-02-26 10:43:45 +0200

    SearchBar: added semantics for controls inside.

--- lib/ui/widgets/SearchBar.dart
+++ lib/ui/widgets/SearchBar.dart
@@ -13,15 +13,21 @@ class SearchBar extends StatelessWidget {
         color: Colors.black26,
 //        color: Color.fromARGB(0, 188, 192, 195),
         child: Row(children: <Widget>[
-          GestureDetector(
-            onTap: () => Navigator.pushNamed(context, '/edit'),
-            child: Image.asset(
-              'images/icon-settings.png',
-              width: 42,
-              height: 42,
-              fit: BoxFit.cover,
-            ),
-          ),
+          Semantics(
+            button: true,
+            excludeSemantics: true,
+            label: "Settings",
+            hint: "Tap to edit application settings",
+            child: GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/edit'),
+              child: Image.asset(
+                'images/icon-settings.png',
+                width: 42,
+                height: 42,
+                fit: BoxFit.cover,
+              ),
+            )
+           ),
           Expanded(child:Padding(
           padding: EdgeInsets.symmetric(horizontal: 3, vertical: 4),
           child:Ink(
@@ -31,15 +37,26 @@ class SearchBar extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(25.0))),
             child:Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                  child: 
+                  Semantics(
+                    textField: true,
+                    label: "Search",
+                    hint: "Input your search text",
                   child: TextField(
                     style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                         border: InputBorder.none
     )               ,
-    )           )
+    )           ))
           ))),
-          Image.asset('images/icon-search.png',
+          Semantics(
+            button: true,
+            excludeSemantics: true,
+            label: "Search",
+            hint: "Tap to start search",
+            child: Image.asset('images/icon-search.png',
               width: 42, height: 42, fit: BoxFit.cover),
+                    )
         ]));
   }
 }
