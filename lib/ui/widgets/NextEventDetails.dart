a73bb935a5126d97260c4b1bb38a7d66034bc867
Mihail Varbanov <mihail.varbanov@inabyte.com>
2019-02-26 10:36:03 +0200

    Next Even Details: wrap some texts & icons in Semantic wrappers.

--- lib/ui/widgets/NextEventDetails.dart
+++ lib/ui/widgets/NextEventDetails.dart
@@ -10,55 +10,66 @@ class NextEventDetails extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Expanded(
-          child: Image.asset(
-            'images/icon-time.png',
-            height: 62,
-            width: 62,
+        Semantics(
+          excludeSemantics: true,
+          hint: "1 hr 20 min until the event",
+          child:Row(children: <Widget>[
+            Image.asset(
+              'images/icon-time.png',
+              height: 62,
+              width: 62,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Time Until',
+                    style: TextStyle(fontSize: 18, fontFamily: 'Avenir'),
+                  ),
+                  Text('1 hr 20 min',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Avenir'))
+                ],
+              ),
+            ),
+        ])),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Semantics(
+            excludeSemantics: true,
+            hint: "15 minutes to alarm",
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'images/icon-clock.png',
+                  height: 50,
+                ),
+                Text(
+                  '15 min',
+                  style: TextStyle(fontFamily: 'Avenir'),
+                )
+              ],
+            ),
           ),
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Time Until',
-                style: TextStyle(fontSize: 18, fontFamily: 'Avenir'),
-              ),
-              Text('1 hr 20 min',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Avenir'))
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                'images/icon-clock.png',
-                height: 50,
-              ),
-              Text(
-                '15 min',
-                style: TextStyle(fontFamily: 'Avenir'),
-              )
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                'images/icon-bycicle.png',
-                height: 50,
-              ),
-              Text(
-                '7 min',
-                style: TextStyle(fontFamily: 'Avenir'),
-              )
-            ],
+          child: Semantics(
+            excludeSemantics: true,
+            hint: "7 minutes to bycycle",
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'images/icon-bycicle.png',
+                  height: 50,
+                ),
+                Text(
+                  '7 min',
+                  style: TextStyle(fontFamily: 'Avenir'),
+                )
+              ],
+            ),
           ),
         ),
       ],
