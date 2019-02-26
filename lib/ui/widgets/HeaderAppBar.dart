69568db1a74df81eb8960787361c4475c9a32763
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-26 10:56:52 +0200

    Add semantics to the Header bar

--- lib/ui/widgets/HeaderAppBar.dart
+++ lib/ui/widgets/HeaderAppBar.dart
@@ -9,14 +9,27 @@ class HeaderAppBar extends AppBar {
   final BuildContext context;
 
   HeaderAppBar({@required this.context})
-      : super(actions: <Widget>[
-          IconButton(
-            icon: Image.asset('images/icon-illinois.png'),
-            onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => WebContentPanel(
-                        url: 'https://illinois.edu', title: 'UIUC'))),
-          )
-        ], centerTitle: true, title: Text('UIUC'));
+      : super(
+            actions: <Widget>[
+              Semantics(
+                excludeSemantics: true,
+                button: true,
+                hint: 'Tap to open Illinois home page',
+                child: IconButton(
+                  icon: Image.asset('images/icon-illinois.png'),
+                  onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => WebContentPanel(
+                              url: 'https://illinois.edu', title: 'UIUC'))),
+                ),
+              )
+            ],
+            centerTitle: true,
+            title: Semantics(
+              excludeSemantics: true,
+              label: 'University of Illinois Urbana-Champaign',
+              header: true,
+              child: Text('UIUC'),
+            ));
 }
