aa9096115b85c8fd297624de3dd8e29cd04209e1
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-26 14:06:59 +0200

    Make white space between row items equal. Remove padding. Fix UI error for small mobile displays

--- lib/ui/widgets/NextEventDetails.dart
+++ lib/ui/widgets/NextEventDetails.dart
@@ -8,68 +8,61 @@ class NextEventDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         Semantics(
-          excludeSemantics: true,
-          hint: "1 hr 20 min until the event",
-          child:Row(children: <Widget>[
-            Image.asset(
-              'images/icon-time.png',
-              height: 62,
-              width: 62,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Time Until',
-                    style: TextStyle(fontSize: 18, fontFamily: 'Avenir'),
-                  ),
-                  Text('1 hr 20 min',
-                      style: TextStyle(fontSize: 30, fontFamily: 'Avenir'))
-                ],
-              ),
-            ),
-        ])),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Semantics(
             excludeSemantics: true,
-            hint: "15 minutes to alarm",
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  'images/icon-clock.png',
-                  height: 50,
-                ),
-                Text(
-                  '15 min',
-                  style: TextStyle(fontFamily: 'Avenir'),
-                )
-              ],
-            ),
+            hint: "1 hr 20 min until the event",
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset(
+                    'images/icon-time.png',
+                    height: 62,
+                    width: 62,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Time Until',
+                        style: TextStyle(fontSize: 18, fontFamily: 'Avenir'),
+                      ),
+                      Text('1 hr 20 min',
+                          style: TextStyle(fontSize: 30, fontFamily: 'Avenir'))
+                    ],
+                  ),
+                ])),
+        Semantics(
+          excludeSemantics: true,
+          hint: "15 minutes to alarm",
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'images/icon-clock.png',
+                height: 50,
+              ),
+              Text(
+                '15 min',
+                style: TextStyle(fontFamily: 'Avenir'),
+              )
+            ],
           ),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Semantics(
-            excludeSemantics: true,
-            hint: "7 minutes to bycycle",
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  'images/icon-bycicle.png',
-                  height: 50,
-                ),
-                Text(
-                  '7 min',
-                  style: TextStyle(fontFamily: 'Avenir'),
-                )
-              ],
-            ),
+        Semantics(
+          excludeSemantics: true,
+          hint: "7 minutes to bycycle",
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'images/icon-bycicle.png',
+                height: 50,
+              ),
+              Text(
+                '7 min',
+                style: TextStyle(fontFamily: 'Avenir'),
+              )
+            ],
           ),
         ),
       ],
