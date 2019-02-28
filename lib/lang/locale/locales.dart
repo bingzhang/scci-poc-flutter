4f6ac08f0fe7e87b07b381172671cb9d6a6c21f4
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-02-28 09:35:36 +0200

    Code realignment according to flutter recommendations.

--- lib/lang/locale/locales.dart
+++ lib/lang/locale/locales.dart
@@ -4,36 +4,39 @@ import 'package:intl/intl.dart';
 
 import 'package:profile_demo/lang/l10n/messages_all.dart';
 
-class AppLocalizations{
-  static Future<AppLocalizations> load(Locale locale) async{
-    final String name = (locale.countryCode == null ||
-        locale.countryCode.isEmpty) ? locale.languageCode : locale.toString();
+class AppLocalizations {
+  static Future<AppLocalizations> load(Locale locale) async {
+    final String name =
+        (locale.countryCode == null || locale.countryCode.isEmpty)
+            ? locale.languageCode
+            : locale.toString();
 
     final String localName = Intl.canonicalizedLocale(name);
 
-    return initializeMessages(localName).then((bool _){
+    return initializeMessages(localName).then((bool _) {
       Intl.defaultLocale = localName;
       return AppLocalizations();
     });
-
   }
 
-  static AppLocalizations of(BuildContext context){
-    AppLocalizations local= Localizations.of<AppLocalizations>(context,AppLocalizations);
+  static AppLocalizations of(BuildContext context) {
+    AppLocalizations local =
+        Localizations.of<AppLocalizations>(context, AppLocalizations);
     return local;
   }
 
-  String get studentHomeGoodMorningText{
-      return Intl.message('Good Morning, Alex!',name:'studentHomeGoodMorningText');
+  String get studentHomeGoodMorningText {
+    return Intl.message('Good Morning, Alex!',
+        name: 'studentHomeGoodMorningText');
   }
 }
 
-class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations>{
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
   const AppLocalizationsDelegate();
 
   @override
   bool isSupported(Locale locale) {
-    return ['en','es','zh'].contains(locale.languageCode);
+    return ['en', 'es', 'zh'].contains(locale.languageCode);
   }
 
   @override
@@ -45,5 +48,4 @@ class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations>{
   bool shouldReload(LocalizationsDelegate<AppLocalizations> old) {
     return false;
   }
-
 }
