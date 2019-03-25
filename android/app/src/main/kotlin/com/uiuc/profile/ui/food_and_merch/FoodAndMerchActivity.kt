f53a8c73b7952b653d5d506a94a7f71279ae12dd
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2019-03-25 16:22:40 +0200

    Manually bring the user back from android fnm content to flutter content

--- android/app/src/main/kotlin/com/uiuc/profile/ui/food_and_merch/FoodAndMerchActivity.kt
+++ android/app/src/main/kotlin/com/uiuc/profile/ui/food_and_merch/FoodAndMerchActivity.kt
@@ -6,6 +6,7 @@ package com.uiuc.profile.ui.food_and_merch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.uiuc.profile.R
 
 class FoodAndMerchActivity : AppCompatActivity() {
@@ -13,5 +14,18 @@ class FoodAndMerchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_and_merch)
+    }
+
+    /**
+     * Manually bring the user back from android content to flutter content
+     */
+    override fun onBackPressed() {
+        val fmHomeFragmentId = R.id.standsFragment
+        val currentDestId = findNavController(R.id.fragment_food_and_merch).currentDestination?.id
+        if (currentDestId == fmHomeFragmentId) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
